c1deabe219699bc858db5d0f3fdd1588e6fea94a
ashutoshdb <ashutoshdubey607@gmail.com>
2022-01-26 11:34:19 +0530

    assigment done

--- assign/12/main/main.c
+++ assign/12/main/main.c
@@ -1,11 +1,81 @@
-// 12. What are the APIs provided by your RTOS for enabling and disabling the interrupts? Write
-// a program to demonstrate this capability?
+// 13. Does your RTOS provide APIs to collect task statistics. If yes, list the statistics parameters
+// that are collected and write a program to display the runtime task statistics?
+
 #include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
-int app_main()
+TaskHandle_t xHandleTaskLog_1;
+TaskHandle_t xHandleTaskLog_2;
+eTaskState x;
+
+void task_1(void *pvparameters)
 {
-    printf("Hello C-DAC\n");
-    return 0;
+    while(1)
+    {
+        printf("TASK 1 RUNNING\n");
+        vTaskDelay(1000/ portTICK_PERIOD_MS);
+    }
 }
+void task_2(void *pvparameters)
+{
+    uint32_t count=0;
+    while(1)
+    {
+        count++;
+        printf("TASK 2 RUNNING\n");
+        vTaskDelay(1000/ portTICK_PERIOD_MS);
+        if(count==5)
+        {
+            vTaskDelete(NULL);
+        }
+    }
+}
+void app_main()
+{
 
-// run 'idf.py -p (PORT) flash'
+    //y=uxTaskGetNumberOfTasks();
+
+    BaseType_t result;
+    
+    result=xTaskCreate(task_1,"task_1",1024,NULL,5,&xHandleTaskLog_1);
+
+    if(result==pdPASS)
+    {
+        printf("task_1 created\n");
+    }
+    result=xTaskCreate(task_2,"task_2",1024,NULL,5,&xHandleTaskLog_2);
+
+    if(result==pdPASS)
+    {
+        printf("task_2 created\n");
+    }
+    while(1)
+    {
+     x=eTaskGetState( xHandleTaskLog_2 );
+     switch(x)
+     {
+         case eRunning:
+             printf("TI IS RUNNING\n");
+             break;
+         case eReady:
+             printf("IT IS READY\n");
+             break;
+         case eDeleted:
+             printf("IT IS DELETED\n");
+             break;
+         case eBlocked:
+             printf("IT IS BLOCKED\n");
+             break;
+         case eSuspended:
+             printf("IT IS SUSPEND\n");
+             break;
+         default:
+             printf("UNKNOW STATE\n");
+             break; 
+        }  
+     printf("STATE :%d\n",x);  
+     vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+
+}
--- assign/readme.md
+++ assign/readme.md
@@ -23,24 +23,5 @@
 
 # 10 Draw a process or task state diagram for the RTOS you are using.
 - DORMANT -> ready state ->running state -> wait state -> ISR
-
-12 incomplte for now
-13. Does your RTOS provide APIs to collect task statistics. If yes, list the statistics parameters
-that are collected and write a program to display the runtime task statistics?
-14. Find the tick frequency configuration for your RTOS.
-15. Create a task to suspend itself after 1200 ms and resume it from another task
-16. Write a RTOS application to demonstrate the use of changing priority
-17. If you RTOS supports idle task hooking, write a program to demonstrate it
-18. Write a RTOS application to demonstrate the use of task to task communication using
-Queue management APIs. Also demonstrate blocking on a queue.
-19. Write a RTOS application to demonstrate the effects of task priorities when sending to and
-receiving from a queue.
-20. Write a RTOS application to demonstrate deferred interrupt processing using binary semaphores
-21. Write a RTOS application to demonstrate counting semaphores to synchronize a task
-22. Write a RTOS application to demonstrate the usage of queues within an interrupt service
-routine
-23. Write a RTOS application to manage resources using mutual exclusion
-24. Write a RTOS application to demonstrate a priority inversion problem. If your RTOS sup-
-ports priority inheritance or priority ceiling, use it to solve the priority inversion problem.
-25. Write a RTOS application to create a software timer that invokes a callback function every
-5 seconds.
+# Find the tick frequency configuration for your RTOS.
+- 100Hz
